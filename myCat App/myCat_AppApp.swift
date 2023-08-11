f7642b847867dbc5905591d6692724be26c89eff
Anand P <ananhci@gmail.com>
2023-08-11 09:50:51 +0400

    Initial Commit

---
+++ myCat App/myCat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCat_AppApp.swift
+//  myCat App
+//
+//  Created by Janarth Anand on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct myCat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
